5fb9804d32424a75bc5b6224a6b48e4cfc641174
Donghyeon Lee <ehdgus001928@gmail.com>
2024-07-18 04:41:48 +0900

    feat: access token 확인 기능 임시 구현

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -1,5 +1,13 @@
 import axios from 'axios';
 
+export const authInstance = axios.create({
+  baseURL: '/api/auth',
+  timeout: 3000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
 export const deployInstance = axios.create({
   baseURL: '/api/deploy',
   timeout: 3000,
@@ -8,10 +16,11 @@ export const deployInstance = axios.create({
   },
 });
 
-export const authInstance = axios.create({
-  baseURL: '/api/auth',
-  timeout: 3000,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
+deployInstance.interceptors.request.use(
+  // if expired
+  // refresh token.
+  // store new access token
+  // and continue request.
+  (config) => config,
+  (error) => Promise.reject(error),
+);
