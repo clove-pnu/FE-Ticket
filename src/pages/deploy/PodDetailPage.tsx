a4d28e40407040cbda8b157dd1677a1632e53d02
Donghyeon Lee <ehdgus001928@gmail.com>
2024-07-18 05:20:31 +0900

    feat: 대시보드 컴포넌트 임시 구현

--- src/pages/deploy/PodDetailPage.tsx
+++ src/pages/deploy/PodDetailPage.tsx
@@ -1,7 +1,10 @@
+import Dashboard from '../../components/deploy/Dashboard';
+
 export default function PodDetailPage() {
   return (
     <main>
       <h1 className="text-xl font-bold">Pod test 0 상세 정보</h1>
+      <Dashboard />
     </main>
   );
 }
