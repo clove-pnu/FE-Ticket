704a7f15f6fa43820926e4e6f777ff9381112f2a
Donghyeon Lee <ehdgus001928@gmail.com>
2024-07-17 20:13:29 +0900

    feat: 배포 API 주소 수정

--- src/apis/deploy.ts
+++ src/apis/deploy.ts
@@ -1,9 +1,9 @@
 import { deployInstance } from './instance';
 
-export async function getPods() {
-  return deployInstance.get('/pods/test2');
+export async function getNamespaces() {
+  return deployInstance.get('/server/list');
 }
 
-export async function deployPod({ templateName }: { templateName:string }) {
-  return deployInstance.post('/deploy', { templateName });
+export async function getPods({ namespace }: { namespace: string }) {
+  return deployInstance.get(`/server/${namespace}/pods`);
 }
