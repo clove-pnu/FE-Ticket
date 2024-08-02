b380b958243c2cae44c9e6ed5ec0164780a95392
Donghyeon Lee <ehdgus001928@gmail.com>
2024-08-02 12:19:49 +0900

    feat: 모니터링 레이아웃 구현

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,9 @@ import LoginPage from './pages/auth/LoginPage';
 import RegisterPage from './pages/auth/SignupPage';
 import { AuthProvider } from './hooks/useAuth';
 import PrivateRoute from './pages/auth/PrivateRoute';
-import PodDetailPage from './pages/deploy/PodDetailPage';
 import TemplatePage from './pages/deploy/TemplatePage';
 import ConcertDeployPage from './pages/deploy/ConcertDeployPage';
 import PlayDetailPage from './pages/deploy/PlayDetailPage';
-import PlayDetailLayout from './components/common/Layout/PlayDetailLayout';
 import PlayMonitorPage from './pages/deploy/PlayMonitorPage';
 import ServerMonitorPage from './pages/deploy/ServerMonitorPage';
 import PlayConfigurationPage from './pages/deploy/PlayConfigurationPage';
@@ -37,15 +35,11 @@ export default function App() {
               <Route path="/owner/deploy/sports" element={<ConcertDeployPage />} />
               <Route path="/owner/deploy/exhibition" element={<ConcertDeployPage />} />
 
-              <Route path="/owner/podDetail/:podName" element={<PodDetailPage />} />
-
               {/* Play Detail (seller) */}
-              <Route element={<PlayDetailLayout />}>
-                <Route path="/owner/playDetail/:pid" element={<PlayDetailPage />} />
-                <Route path="/owner/playMonitor/:pid" element={<PlayMonitorPage />} />
-                <Route path="/owner/serverMonitor/:pid" element={<ServerMonitorPage />} />
-                <Route path="/owner/playConfiguration/:pid" element={<PlayConfigurationPage />} />
-              </Route>
+              <Route path="/owner/playDetail/:pid" element={<PlayDetailPage />} />
+              <Route path="/owner/playMonitor/:pid" element={<PlayMonitorPage />} />
+              <Route path="/owner/serverMonitor/:pid" element={<ServerMonitorPage />} />
+              <Route path="/owner/playConfiguration/:pid" element={<PlayConfigurationPage />} />
 
               {/* Play Ticketing */}
               <Route path="/play/:pid/ticketing" element={<PlayTicketingPage />} />
--- src/pages/deploy/PlayDetailPage.tsx
+++ src/pages/deploy/PlayDetailPage.tsx
@@ -1,3 +1,4 @@
+import { useParams } from 'react-router-dom';
 import CategoryTitle from '../../components/common/CategoryTitle';
 import OwnerTab from '../../components/deploy/OwnerTab';
 import PlayDetail from '../../components/deploy/PlayDetail';
@@ -16,8 +17,14 @@ const mockdata: DeployedPlayDetail = {
 };
 
 export default function PlayDetailPage() {
+  const { pid } = useParams();
   return (
     <main>
+      <CategoryTitle>공연 상세 정보</CategoryTitle>
+      <OwnerTab
+        pid={Number(pid)}
+        current="PlayDetail"
+      />
       <PlayDetail
         pid={mockdata.pid}
         thumbnailUrl={mockdata.thumbnailUrl}
